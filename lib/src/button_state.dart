da2d58e4b6a45cc9fcf28e98231812df688c5dfd
Bharath <bt9944@gmail.com>
2021-08-29 12:16:27 +0530

    properties documentation

--- lib/src/button_state.dart
+++ lib/src/button_state.dart
@@ -1,29 +1,57 @@
 import 'package:flutter/material.dart';
+import 'multi_state_button_controller.dart';
 
 class ButtonState {
+  /// This is the name used to identify the state of the button.
+  /// This name is used to change the state using [MultiStateButtonController]
   final String stateName;
-  final VoidCallback? onPressed;
-  final Color color;
   final Widget child;
+  final VoidCallback? onPressed;
+
+  /// Size of the button
   final Size? size;
-  final BoxDecoration? boxDecoration;
+
+  /// Color of the button.
+  /// This should be null if boxDecoration is not null.
+  final Color color;
+
+  /// Decorate the button with color, border radius, box shadow, gradient etc...
+  final BoxDecoration? decoration;
+
+  /// Used to align the child.
+  /// Defaults to [Alignment.center]
   final AlignmentGeometry alignment;
+
+  /// If descendant of the button is a Text child, this textStle is used to enable transition animtation between two states.
   final TextStyle? textStyle;
+
+  /// The transformation matrix to apply before painting the button.
   final Matrix4? transform;
+
+  /// The clip behavior when [decoration] is not null.
   final Clip clipBehavior;
+
+  /// The decoration to paint in front of the child.
   final Decoration? foregroundDecoration;
+
+  /// SubRoutine to run on transition animation ends.
   final VoidCallback? onAnimationEnd;
+
+  /// The alignment of the child, relative to the size of the button, if [transform] is specified.
+  /// When [transform] is null, the value of this property is ignored.
   final AlignmentGeometry? transformAlignment;
 
-  ButtonState({
+  /// Button with multiple states.
+  /// Note: For better text transition between states,
+  const ButtonState({
     required this.stateName,
-    this.onPressed,
     this.child = const SizedBox(),
-    this.textStyle,
+    this.onPressed,
     this.size,
     this.color = Colors.blue,
-    this.boxDecoration,
+    this.decoration,
     this.alignment = Alignment.center,
+    this.textStyle,
     this.transform,
     this.clipBehavior = Clip.none,
     this.foregroundDecoration,
--- lib/src/multi_state_button_controller.dart
+++ lib/src/multi_state_button_controller.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/foundation.dart';
+import 'package:multi_state_button/multi_state_button.dart';
 
 class MultiStateButtonController {
+  /// Pass the [ButtonState.stateName] to set the initialState.
   MultiStateButtonController({required String initialStateName}) : _buttonStateName = ValueNotifier(initialStateName);
 
   final ValueNotifier<String> _buttonStateName;
   ValueNotifier<String> get buttonStateName => _buttonStateName;
+
+  /// Pass the [ButtonState.stateName] to set the current button state.
   set setButtonState(String stateName) => _buttonStateName.value = stateName;
 }
